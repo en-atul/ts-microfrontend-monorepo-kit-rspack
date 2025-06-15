dcb6b7b169b586ffac710d4a6c9fb404633bfcd2
en-atul <atkr5496@gmail.com>
2025-06-15 21:41:51 +0530

    bundle analyzer added

--- packages/rspack-config/src/rspack.prod.js
+++ packages/rspack-config/src/rspack.prod.js
@@ -2,6 +2,7 @@ import path from 'path';
 import { getFilePaths } from './utils.js';
 import { ModuleFederationPlugin } from '@module-federation/enhanced/rspack';
 import { rspack } from '@rspack/core';
+import { RsdoctorRspackPlugin } from '@rsdoctor/rspack-plugin';
 
 export default ({ baseUrl, configs }) => {
 	const { __dirname } = getFilePaths(baseUrl);
@@ -22,7 +23,7 @@ export default ({ baseUrl, configs }) => {
 			filename: '[name].[contenthash].js',
 			path: __dirname + '/dist',
 			publicPath: configs.publicPath,
-			clean: true
+			clean: true,
 		},
 		plugins: [
 			new ModuleFederationPlugin({
@@ -35,6 +36,11 @@ export default ({ baseUrl, configs }) => {
 			new rspack.CssExtractRspackPlugin({
 				filename: '[name].[contenthash].css',
 			}),
+			process.env.RSDOCTOR &&
+				new RsdoctorRspackPlugin({
+					port: 8080,
+					mode: 1,
+				}),
 		],
 	};
 };
